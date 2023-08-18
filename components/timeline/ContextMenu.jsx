35c723a8f6800f4c70a86df5b6c73190aadacbe8
Warren Green <s.green.warren@gmail.com>
2023-08-17 21:46:55 -0500

    inngest processing

--- components/timeline/ContextMenu.jsx
+++ components/timeline/ContextMenu.jsx
@@ -1,5 +1,7 @@
 import { TrashIcon } from '@radix-ui/react-icons'
 import {forwardRef} from 'react' 
+import { Button } from '@radix-ui/themes';
+
 const ContextMenu = forwardRef(({ x, y,visible, segment}, ref) => {
       return (
           <Button  ref={ref} style={{ display:visible, position: "fixed", top: y, left: x, alignItems: "center", display: "flex"}} onClick={() =>{deleteSegment();setVisible("none")}}>Delete <TrashIcon/></Button>
