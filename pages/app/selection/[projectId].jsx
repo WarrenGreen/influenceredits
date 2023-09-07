08c197bf633fd80d7c5a22d8414e3c1c3306977d
Warren Green <s.green.warren@gmail.com>
2023-09-06 22:51:27 -0500

    auth

--- pages/app/selection/[projectId].jsx
+++ pages/app/selection/[projectId].jsx
@@ -179,9 +179,6 @@ export default function Editor({session, projectVideos, projectSegments, project
   }
   const [currentVideo, setCurrentVideo] = useState(currentVideo_beta)
 
-  // When rendering client side don't display anything until loading is complete
-  if (typeof window !== 'undefined' && loading) return null
-
   // If no session exists, display access denied message
   if (!session) { return  <Layout><AccessDenied/></Layout> }
 
