565e8a3e1e822f0ed87408e29e9a6dbdb466d26f
Warren Green <s.green.warren@gmail.com>
2023-10-12 21:01:50 -0500

    swap index items

--- pages/index.jsx
+++ pages/index.jsx
@@ -20,9 +20,10 @@ export default function Home() {
       <main>
         <Hero />
         <PrimaryFeatures />
-        <CallToAction />
         {/*<Testimonials />*/}
         <Pricing />
+        <CallToAction />
+
       </main>
       <Footer />
     </>
